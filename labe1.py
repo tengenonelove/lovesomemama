64eebe08cf422c78152fb6300b2f93fd374ba658
tengenonelove <112887261+tengenonelove@users.noreply.github.com>
2022-09-24 18:09:56 +0300

    Add files via upload

---
+++ labe1.py
@@ -0,0 +1,16 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x = 10
+>>> str_x = str(x)
+>>> str_x
+'10'
+>>> num_x = len(str_x)
+>>> num_x
+2
+>>> x =100500
+>>> str_x = str(x)
+>>> str_x
+'100500'
+>>> num_x = len(str_x)
+>>> num_x
+6
